2b2ca9995132ac9ffcac428bb657ad51a813a3d9
Team2481 <kyle@team2481.com>
2014-01-31 19:04:46 -0600

    Created Juke Command

---
+++ r2014/Commands/JukeCommand.cpp
@@ -0,0 +1,50 @@
+#include "JukeCommand.h"
+
+JukeCommand::JukeCommand(bool jukeRight){
+	Requires (driveTrain);
+	isRight = jukeRight;
+}
+	void JukeCommand::Initialize(){
+		
+	}
+	void JukeCommand::Execute(){
+		if (isRight == true){
+			if (TimeSinceInitialized() < .25){
+				driveTrain->Crab(-1,0,0,false);
+			}
+			else if (TimeSinceInitialized() < .75){
+				driveTrain->Crab(1,0,0,false);
+			}
+			else if (TimeSinceInitialized() < 1.75){
+				driveTrain->Crab(.25,1,.5,false);
+			}
+		}
+		else{
+			if (TimeSinceInitialized() < .25){
+				driveTrain->Crab(1,0,0,false);
+			}
+			else if (TimeSinceInitialized() < .75){
+				driveTrain->Crab(-1,0,0,false);
+			}
+			else if (TimeSinceInitialized() < 1.75){
+				driveTrain->Crab(-.25,1,-.5,false);
+			}
+		}
+		
+	}
+	bool JukeCommand::IsFinished(){
+		if (TimeSinceInitialized() > 2.25){
+			return true;
+		}
+	}
+	void JukeCommand::End(){
+		driveTrain->Stop();
+	}
+	void JukeCommand::Interrupted(){
+		
+	}
+
+
+JukeCommand::~JukeCommand()
+{
+}
---
+++ r2014/Commands/JukeCommand.h
@@ -0,0 +1,19 @@
+#ifndef JUKECOMMAND_H_
+#define JUKECOMMAND_H_
+
+#include "../CommandBase.h"
+
+class JukeCommand: public CommandBase {
+public:
+	JukeCommand(bool isRight);
+	virtual ~JukeCommand();
+	virtual void Initialize();
+	virtual void Execute();
+	virtual bool IsFinished();
+	virtual void End();
+	virtual void Interrupted();
+private:
+	bool isRight;
+};
+
+#endif /*JUKECOMMAND_H_*/
