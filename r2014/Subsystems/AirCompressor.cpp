43004903fd6e27de57751c95701bc9b303f00731
FRC Team 2481 <kyle@team2481.com>
2014-02-12 17:20:17 -0600

    Air Compressor

---
+++ r2014/Subsystems/AirCompressor.cpp
@@ -0,0 +1,27 @@
+/*
+ * AirCompressor.cpp
+ *
+ *  Created on: Jan 24, 2013
+ *      Author: Team2481
+ */
+
+#include "AirCompressor.h"
+
+AirCompressor::AirCompressor(UINT32 sensor, UINT32 spike) : Subsystem("AirCompressor") {
+	// TODO Auto-generated constructor stub
+	compress = new Compressor(sensor, spike);
+	compress->Start();
+}
+
+AirCompressor::~AirCompressor() {
+	// TODO Auto-generated destructor stub
+	delete compress;
+}
+
+void AirCompressor::Stop() {
+	compress->Stop();
+}
+
+void AirCompressor::Start() {
+	compress->Start();
+}
---
+++ r2014/Subsystems/AirCompressor.h
@@ -0,0 +1,24 @@
+/*
+ * AirCompressor.h
+ *
+ *  Created on: Jan 24, 2013
+ *      Author: Team2481
+ */
+
+#ifndef AIRCOMPRESSOR_H_
+#define AIRCOMPRESSOR_H_
+
+#include "WPILib.h"
+
+class AirCompressor: public Subsystem {
+private:
+	Compressor* compress;
+public:
+	AirCompressor(UINT32 sensor, UINT32 spike);
+	virtual ~AirCompressor();
+	
+	void Stop();
+	void Start();
+};
+
+#endif /* AIRCOMPRESSOR_H_ */
