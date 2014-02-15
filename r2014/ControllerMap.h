cbff15ed26418de26c1a0630239f9299fe0ca87d
Team2481 <kyle@team2481.com>
2014-02-15 12:35:36 -0600

    Added stick and object allocation to defines

--- r2014/ControllerMap.h
+++ r2014/ControllerMap.h
@@ -5,36 +5,35 @@
 
 
 //Tune Stick
-#define FR_OFFSET_BUTTON 			XboxController::xbBButton
-#define FL_OFFSET_BUTTON 			XboxController::xbYButton
-#define BR_OFFSET_BUTTON			XboxController::xbAButton
-#define BL_OFFSET_BUTTON 			XboxController::xbXButton
+#define FR_OFFSET_BUTTON			new JoystickButton(tuningStick, XboxController::xbBButton)
+#define FL_OFFSET_BUTTON 			new JoystickButton(tuningStick, XboxController::xbYButton)
+#define BR_OFFSET_BUTTON			new JoystickButton(tuningStick, XboxController::xbAButton)
+#define BL_OFFSET_BUTTON 			new JoystickButton(tuningStick, XboxController::xbXButton)
 
 //Driver Stick
-#define CALIBRATE_COMPASS_BUTTON 	XboxController::xbZAxis
-#define TOGGLE_RIGHT_EAR_BUTTON 	XboxController::xbRightBumper
-#define TOGGLE_LEFT_EAR_BUTTON 		XboxController::xbLeftBumper
+#define CALIBRATE_COMPASS_BUTTON 	new AnalogJoystickButton(driverStick, XboxController::xbZAxis, -.5)
+#define SET_FIELD_OFFSET_BUTTON 	new JoystickButton(driverStick, XboxController::xbStartButton)
+#define RESET_GYRO_BUTTON 			new JoystickButton(driverStick, XboxController::xbBackButton)
+#define TOGGLE_FIELD_CENTRIC_BUTTON new AnalogJoystickButton(driverStick, XboxController::xbZAxis, -.5)
+#define TOGGLE_FORWARD_BUTTON		new JoystickButton(driverStick, XboxController::xbAButton)
 
-#define SET_FIELD_OFFSET_BUTTON 	XboxController::xbStartButton
-#define RESET_GYRO_BUTTON 			XboxController::xbBackButton
-
-#define TOGGLE_FIELD_CENTRIC_BUTTON XboxController::xbZAxis
+#define TURN_ON_INTAKE_BUTTON 		new JoystickButton(driverStick, XboxController::xbRightBumper)
+#define TURN_OFF_INTAKE_BUTTON 		new JoystickButton(driverStick, XboxController::xbLeftBumper)
 
 //Aux Stick
-#define RETRACT_SHOOTER_BUTTON 		XboxController::xbRightYAxis
-#define RELEASE_SHOOTER_BUTTON 		XboxController::xbRightYAxis
-#define AUTO_SET_SHOOTER_BUTTON		XboxController::xbAButton
-#define FIRE_BUTTON 				XboxController::xbZAxis
-//#define MANUAL_LATCH_BUTTON 		XboxController::xbBButton
-#define TURN_ON_INTAKE_BUTTON 		XboxController::xbRightBumper
-#define TURN_OFF_INTAKE_BUTTON 		XboxController::xbLeftBumper
-#define VOMIT_BUTTON 				XboxController::xbBButton
-#define TOGGLE_INTAKE_READY_BUTTON 	XboxController::xbLeftStickClick
-#define AUTO_COCK_SHOOTER_BUTTON 	XboxController::xbZAxis
-#define SHOOTER_TO_THROTTLE_BUTTON	XboxController::xbXButton
-//#define INC_SHOOTER_BUTTON			XboxController::xbYButton
-//#define DEC_SHOOTER_BUTTON			XboxController::xbXButton
+#define RETRACT_SHOOTER_BUTTON 		new AnalogJoystickButton(auxStick, XboxController::xbRightYAxis, -.5)
+#define RELEASE_SHOOTER_BUTTON 		new AnalogJoystickButton(auxStick, XboxController::xbRightYAxis, .5)
+#define AUTO_SET_SHOOTER_BUTTON		new JoystickButton (auxStick, XboxController::xbAButton)
+#define FIRE_BUTTON_NUM				XboxController::xbZAxis
+#define FIRE_BUTTON 				new AnalogJoystickButton(auxStick, FIRE_BUTTON_NUM, -.5)
 
+#define VOMIT_BUTTON 				new JoystickButton(auxStick, XboxController::xbBButton)
+#define TOGGLE_INTAKE_READY_BUTTON 	new JoystickButton(auxStick, XboxController::xbLeftStickClick)
 
+#define TOGGLE_RIGHT_EAR_BUTTON 	new JoystickButton(auxStick, XboxController::xbRightBumper)
+#define TOGGLE_LEFT_EAR_BUTTON 		new JoystickButton(auxStick, XboxController::xbLeftBumper)
+
+#define AUTO_COCK_SHOOTER_BUTTON 	new AnalogJoystickButton (auxStick, XboxController::xbZAxis, .5)
+#define SHOOTER_TO_THROTTLE_BUTTON	new JoystickButton(auxStick, XboxController::xbXButton)
 
 #endif 
--- r2014/OI.cpp
+++ r2014/OI.cpp
@@ -25,6 +25,7 @@
 #include "Commands/DecShooterSetPointCommand.h"
 #include "Commands/SetShooterToThrottleCommand.h"
 #include "Commands/ToggleFieldCentricCommand.h"
+#include "Commands/ToggleForwardCommand.h"
 
 OI::OI() {
 	// Process operator interface input here.
@@ -33,23 +34,23 @@ OI::OI() {
 	tuningStick = new Joystick2481(TUNE_STICK_PORT);
 	throttleStick = new Joystick(THROTTLE_STICK_PORT);
 	
-	FROffsetButton = new JoystickButton(tuningStick, FR_OFFSET_BUTTON);
+	FROffsetButton = FR_OFFSET_BUTTON;
 	FROffsetButton->WhenPressed(new SetEncoderOffsetCommand(FRENCODER));
 //	FROffsetButton->WhenPressed(new SetICommand(true));
 	
-	FLOffsetButton = new JoystickButton(tuningStick, FL_OFFSET_BUTTON);
+	FLOffsetButton = FL_OFFSET_BUTTON;
 	FLOffsetButton->WhenPressed(new SetEncoderOffsetCommand(FLENCODER));
 //	FLOffsetButton->WhenPressed(new SetPCommand(true));
 	
-	BROffsetButton = new JoystickButton(tuningStick, BR_OFFSET_BUTTON);
+	BROffsetButton = BR_OFFSET_BUTTON;
 	BROffsetButton->WhenPressed(new SetEncoderOffsetCommand(BRENCODER));
 //	BROffsetButton->WhenPressed(new SetICommand(false));
 	
-	BLOffsetButton = new JoystickButton(tuningStick, BL_OFFSET_BUTTON);
+	BLOffsetButton = BL_OFFSET_BUTTON;
 	BLOffsetButton->WhenPressed(new SetEncoderOffsetCommand(BLENCODER));
 //	BLOffsetButton->WhenPressed(new SetPCommand(false));
 	
-	CalibrateCompassButton = new AnalogJoystickButton(driverStick, CALIBRATE_COMPASS_BUTTON, -.5);
+	CalibrateCompassButton = CALIBRATE_COMPASS_BUTTON;
 	CalibrateCompassButton->WhenPressed(new CalibrateCompassCommand());	
 	/*
 	RightJukeButton = new JoystickButton(driverStick, XboxController::xbRightBumper); 
@@ -58,47 +59,47 @@ OI::OI() {
 	LeftJukeButton = new JoystickButton(driverStick, XboxController::xbLeftBumper);
 	LeftJukeButton->WhenPressed(new JukeCommand(false));
 	*/
-	SetFieldOffsetButton = new JoystickButton(driverStick, SET_FIELD_OFFSET_BUTTON);
+	SetFieldOffsetButton = SET_FIELD_OFFSET_BUTTON;
 	SetFieldOffsetButton->WhenPressed(new SetFieldOffsetCommand());	
 	
-	ResetGyroButton = new JoystickButton(driverStick, RESET_GYRO_BUTTON);
+	ResetGyroButton = RESET_GYRO_BUTTON;
 	ResetGyroButton->WhenPressed(new ResetGyroCommand());	
 	
 	
-	RetractShooterButton = new AnalogJoystickButton(auxStick, RETRACT_SHOOTER_BUTTON, -.5);
+	RetractShooterButton = RETRACT_SHOOTER_BUTTON;
 	RetractShooterButton->WhileHeld(new RetractShooterCommand());	
 	
-	ReleaseShooterButton = new AnalogJoystickButton(auxStick, RELEASE_SHOOTER_BUTTON, .5);
+	ReleaseShooterButton = RELEASE_SHOOTER_BUTTON;
 	ReleaseShooterButton->WhileHeld(new ReleaseShooterCommand());	
 
-	ManualFireButton = new AnalogJoystickButton(auxStick, FIRE_BUTTON, -.5);
+	ManualFireButton = FIRE_BUTTON;
 	ManualFireButton->WhenPressed(new FireCommandGroup());	
 	
 //	ManualLatchButton = new JoystickButton(auxStick, MANUAL_LATCH_BUTTON);
 //	ManualLatchButton->WhenPressed(new ManualLatchShooterCommand());
 	
-	TurnOnIntakeButton = new JoystickButton(driverStick, TURN_ON_INTAKE_BUTTON);
+	TurnOnIntakeButton = TURN_ON_INTAKE_BUTTON;
 	TurnOnIntakeButton->WhenPressed(new TurnOnIntakeCommand());
 	
-	TurnOffIntakeButton = new JoystickButton(driverStick, TURN_OFF_INTAKE_BUTTON);
+	TurnOffIntakeButton = TURN_OFF_INTAKE_BUTTON;
 	TurnOffIntakeButton->WhenPressed(new TurnOffIntakeCommand());
 	
-	SpitOutButton = new JoystickButton(auxStick, VOMIT_BUTTON);
+	SpitOutButton = VOMIT_BUTTON;
 	SpitOutButton->WhileHeld(new SpitOutBallCommand());
 	
-	ToggleLeftEarButton = new JoystickButton(auxStick, TOGGLE_LEFT_EAR_BUTTON);
+	ToggleLeftEarButton = TOGGLE_LEFT_EAR_BUTTON;
 	ToggleLeftEarButton->WhenPressed(new ToggleLeftEarCommand());
 	
-	ToggleRightEarButton = new JoystickButton(auxStick, TOGGLE_RIGHT_EAR_BUTTON);
+	ToggleRightEarButton = TOGGLE_RIGHT_EAR_BUTTON;
 	ToggleRightEarButton->WhenPressed(new ToggleRightEarCommand());
 	
-	ToggleIntakeReadyButton = new JoystickButton(auxStick, TOGGLE_INTAKE_READY_BUTTON);
+	ToggleIntakeReadyButton = TOGGLE_INTAKE_READY_BUTTON;
 	ToggleIntakeReadyButton->WhenPressed(new ToggleIntakeReadyCommand());
 	
-	AutoCockShooterButton = new AnalogJoystickButton (auxStick, AUTO_COCK_SHOOTER_BUTTON, .5);
+	AutoCockShooterButton = AUTO_COCK_SHOOTER_BUTTON;
 	AutoCockShooterButton->WhenPressed(new AutoCockShooterCommand());
 	
-	AutoSetShooterButton = new JoystickButton (auxStick, AUTO_SET_SHOOTER_BUTTON);
+	AutoSetShooterButton = AUTO_SET_SHOOTER_BUTTON;
 	AutoSetShooterButton->WhileHeld(new AutoSetShooterCommand());
 	
 //	IncShooterButton = new JoystickButton (auxStick, INC_SHOOTER_BUTTON);
@@ -107,11 +108,14 @@ OI::OI() {
 //	DecShooterButton = new JoystickButton (auxStick, DEC_SHOOTER_BUTTON);
 //	DecShooterButton->WhenPressed(new DecShooterSetPointCommand());
 	
-	ShooterToThrottleButton = new JoystickButton(auxStick, SHOOTER_TO_THROTTLE_BUTTON);
+	ShooterToThrottleButton = SHOOTER_TO_THROTTLE_BUTTON;
 	ShooterToThrottleButton->WhenPressed(new SetShooterToThrottleCommand());
 	
-	ToggleFieldCentricButton = new AnalogJoystickButton(driverStick, TOGGLE_FIELD_CENTRIC_BUTTON, -.5);
+	ToggleFieldCentricButton = TOGGLE_FIELD_CENTRIC_BUTTON;
 	ToggleFieldCentricButton->WhileHeld(new ToggleFieldCentricCommand());
+	
+	ToggleForwardButton = TOGGLE_FORWARD_BUTTON;
+	ToggleForwardButton->WhenPressed(new ToggleForwardCommand());
 	
 	SmartDashboard::PutData(new ResetGyroCommand());
 }
