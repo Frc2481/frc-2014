c70ff1b8146210b96a90e71a83ba21d36be8f131
FRC Team 2481 <kyle@team2481.com>
2014-02-18 18:37:09 -0600

    Added try/catch to camera for getting isHotTarget.

--- r2014/Subsystems/Camera2481.cpp
+++ r2014/Subsystems/Camera2481.cpp
@@ -11,9 +11,12 @@ Camera2481::Camera2481() : Subsystem("Camera2481"){
 	
 }
 
-bool Camera2481::HasTarget(){	
-	printf("Hot Target: %f \n", SmartDashboard::GetNumber("HOTTARGET"));
-	return SmartDashboard::GetBoolean("HOTTARGET");
+bool Camera2481::HasTarget(){
+	try{
+//		printf("Hot Target: %f \n", SmartDashboard::GetNumber("HOTTARGET"));
+		return SmartDashboard::GetBoolean("HOTTARGET");
+	} 
+	catch(int e){}
 }
 
 Camera2481::~Camera2481() {
