6185cb770a77721bebf38ec4165dc0046cb325e9
FRC Team 2481 <kyle@team2481.com>
2014-03-01 14:25:57 -0600

    CIR ultrasonic threading

--- r2014/Components/Ultrasonic2481.cpp
+++ r2014/Components/Ultrasonic2481.cpp
@@ -8,15 +8,19 @@
 #include "Ultrasonic2481.h"
 
 Ultrasonic2481::Ultrasonic2481(uint32_t ultrasonicChannel) : 
-	ultrasonic(new AnalogChannel(ultrasonicChannel)){
+			ultrasonic(new AnalogChannel(ultrasonicChannel)),
+			ultrasonicSemaphore(semMCreate(SEM_Q_PRIORITY)),
+			ultrasonicUpdate(new Notifier(Ultrasonic2481::LoopPeriodic, this)),
+			distance(0){
+
+	ultrasonicUpdate->StartPeriodic(.01);
 }
 
 Ultrasonic2481::~Ultrasonic2481() {
 	// TODO Auto-generated destructor stub
 }
 float Ultrasonic2481::GetDistance(){
-	voltageAccum.add(ultrasonic->GetAverageVoltage());
-	return voltageAccum.avg() * inchesPerVolt;
+	return distance;
 }
 void Ultrasonic2481::SetInchesPerVolt(float inchesPerVolt){
 	this->inchesPerVolt = inchesPerVolt;
@@ -24,4 +28,12 @@ void Ultrasonic2481::SetInchesPerVolt(float inchesPerVolt){
 float Ultrasonic2481::GetRawVoltage(){
 	voltageAccum.add(ultrasonic->GetVoltage());
 	return voltageAccum.avg();
+}
+void Ultrasonic2481::Update(){
+	voltageAccum.add(ultrasonic->GetAverageVoltage());
+	distance  = voltageAccum.avg() * inchesPerVolt;
+}
+void Ultrasonic2481::LoopPeriodic(void* instance) {
+	Ultrasonic2481* ultrasonic = (Ultrasonic2481*) instance;
+	ultrasonic->Update();
 }
--- r2014/Components/Ultrasonic2481.h
+++ r2014/Components/Ultrasonic2481.h
@@ -15,13 +15,18 @@ class Ultrasonic2481 {
 private:
 	AnalogChannel *ultrasonic;
 	float inchesPerVolt;
-	RollingAccumulator<float, 25> voltageAccum;
+	RollingAccumulator<float, 30> voltageAccum;
+	SEM_ID ultrasonicSemaphore;
+	Notifier* ultrasonicUpdate;
+	float distance;
 public:
 	Ultrasonic2481(uint32_t ultrasonicChannel);
 	virtual ~Ultrasonic2481();
 	float GetDistance();
 	void SetInchesPerVolt(float inchesPerVolt);
 	float GetRawVoltage();
+	void Update();
+	static void LoopPeriodic(void *controller);
 };
 
 #endif /* ULTRASONIC2481_H_ */
