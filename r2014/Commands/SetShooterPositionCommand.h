e5cf0849c8c60d5be43fd15ada7718e0d5f00182
FRC Team 2481 <kyle@team2481.com>
2014-03-01 14:28:06 -0600

    CIR set shooter postion command

---
+++ r2014/Commands/SetShooterPositionCommand.h
@@ -0,0 +1,38 @@
+#ifndef SetSHOOTERPOSITIONCOMMAND_H
+#define SetSHOOTERPOSITIONCOMMAND_H
+
+#include "../CommandBase.h"
+
+/**
+ *
+ *
+ * @author ExampleAuthor
+ */
+class SetShooterPositionCommand : public CommandBase {
+private:
+	float distance;
+	bool earsUp;
+public:
+	SetShooterPositionCommand(float setDistance, bool setEarsUp){
+		distance = setDistance;
+		earsUp = setEarsUp;
+	}
+	virtual void Initialize(){
+		printf(" %f %d \n", distance , earsUp);
+		shooter->SetPosition(distance, earsUp);
+		
+	}
+	virtual void Execute(){
+	}
+	virtual bool IsFinished(){
+		return shooter->OnTarget();
+	}
+	virtual void End(){
+		
+	}
+	virtual void Interrupted(){
+		End();
+	}
+};
+
+#endif
